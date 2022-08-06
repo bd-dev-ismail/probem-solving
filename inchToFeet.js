6b6c956f738901ced6e893eca2db1147c327c40b
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-06 12:02:54 +0600

    MileToKmFinder & InchToFeetFinder Function

---
+++ inchToFeet.js
@@ -0,0 +1,8 @@
+//Inch to Feet to Converter
+function userFeet(userInch){
+    const feet = userInch /12;
+    return feet;
+}
+const userInch = 84;
+// const feetFinding = userFeet(userInch);
+console.log('Your Feet is: ', userFeet(userInch));
---
+++ mileToKm.js
@@ -0,0 +1,8 @@
+// ! Mile to 1.60934Km
+//Mile to kilometer Finder
+function kilometerFinder(mile){
+    const km = mile * 1.60934;
+    return km;
+}
+const userMile = 10;
+console.log('User Entry Kilometer is: ', kilometerFinder(userMile));
