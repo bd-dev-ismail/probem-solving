9801c80e00f7e6a0783cdce5c77e4b7e2ba375c6
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-06 12:11:05 +0600

    Even & Odd Finding Function

---
+++ isEven.js
@@ -0,0 +1,20 @@
+//Even Finder Function
+function isEven(number){
+    if(number % 2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(98));
+//Odd Finder Function
+function isOdd(num){
+    if(num % 2 ===1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isOdd(175));
